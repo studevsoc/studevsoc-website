1b169af5d8ba067cb45e56bbcc224743ac92ed9b
Ajmal Aju <dev@ajmlaju.com>
2018-11-14 18:10:02 +0530

    add homepage

--- sds/urls.py
+++ sds/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('sdshome/',include('sdshome.urls'))
+    path('', views.index, name="index"),
 ]
